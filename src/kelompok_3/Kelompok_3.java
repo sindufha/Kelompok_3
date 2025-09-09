04682c908ed740376760d31a5cfc0f6e8b391d2b
sindufha <sindufha215@gmail.com>
2025-09-09 16:28:34 +0700

    1

--- src/kelompok_3/Kelompok_3.java
+++ src/kelompok_3/Kelompok_3.java
@@ -14,7 +14,7 @@ public class Kelompok_3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Fadli");
     }
     
 }
